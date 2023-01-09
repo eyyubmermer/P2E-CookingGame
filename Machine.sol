ae61ba0f947c6065d62b0dc748c3895b91f82ab3
Eyyub Ensar MERMER <98224771+eyyubmermer@users.noreply.github.com>
2023-01-09 22:04:02 +0300

    Create Machine.sol

---
+++ Machine.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract Machine is ERC721, Ownable {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIdCounter;
+
+    constructor() ERC721("Machine", "Mch") {}
+
+    function safeMint(address to) public onlyOwner {
+        require(balanceOf(msg.sender) == 0, "you already have a machine");
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+    }
+}
