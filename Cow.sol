62b9ae4fcd39e50cc57405a37072e221f744cbd0
Eyyub Ensar MERMER <98224771+eyyubmermer@users.noreply.github.com>
2023-02-25 22:00:55 +0300

    Update and rename Machine.sol to Cow.sol

--- Cow.sol
+++ Cow.sol
@@ -5,17 +5,28 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract Machine is ERC721, Ownable {
+contract Cow is ERC721, Ownable {
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
 
-    constructor() ERC721("Machine", "Mch") {}
+    mapping(uint => uint) public lastMilkedTime;
+
+    constructor() ERC721("Cow", "Cow") {}
 
     function safeMint(address to) public onlyOwner {
-        require(balanceOf(to) == 0, "you already have a machine");
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(to, tokenId);
     }
+
+    function setMilkingTime(uint _id, uint _time) public onlyOwner {
+        lastMilkedTime[_id] = _time;
+    }
+
+    function viewMilkingTime(uint _id) public view returns(uint) {
+        return lastMilkedTime[_id];
+    }
+
+
 }
